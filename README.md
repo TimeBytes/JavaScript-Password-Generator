5cc4d763b00778278e836c3f921dc18d6bf59793
TimeBytes <123673532+TimeBytes@users.noreply.github.com>
2023-03-19 22:15:31 -0400

    finished javascript and added comments

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# JavaScript-RPS-Game
+# JavaScript-Password-Generator
+
+## Description
+
+Webpage using Javascript to generate a random password that contains lowercase, uppercase, numbers and/or symbols  
+This project is to incorporate javascript, html and CSS.  
+Randomly generates a strong and secure password for the user.
+Learned how to incorporate javascript into a webpage.
+
+## Usage
+
+When on the webpage, press the Generate Password button to generate a password. There are a series of prompts/confirms the user will have to provide before the password is generated.
+
+User will have to provide, how many characters they would like their password to be  
+User will have to confirm what types of characters they would like in their password (lowercase, uppercase, numbers and/or symbols)(a minimum of 1 type has to be selected).
+Password will then generate in the textbox
--- script.js
+++ script.js
@@ -5,48 +5,90 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
 
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function passwordLengthValidation(){
-	var userInput = prompt("How long would you like your password to be? (Length between 8 - 128 characters");
-	if(isNaN(userInput) == false && userInput >= 8 && userInput <= 128){
-		return userInput;
-	}
-	else{
-		alert("Invalid entry, please try again (Pick a number between 8 - 128");
-		passwordLengthValidation();
-	}
-}
-
-function toInclude(){
-var includeLowerCase = confirm("Would you like to include lowercase characters in your password?");
-var includeUpperCase = confirm("Would you like to include uppercase characters in your password?");
-var includeNumbers = confirm("Would you like to include numbers in your password?");
-var includeSymbols = confirm("Would you like to include symbols in your password?");
- if (!includeLowerCase || !includeUpperCase || !includeNumbers || !includeSymbols)
- 	{
-		alert("Please make sure at least one type is included");
-		toInclude();
-	}
-	
-
-}
-
-var passwordLength = passwordLengthValidation();
-toInclude();
-
-
-
-function generatePassword(){
-	var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-	var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	var numbers = "0123456789";
-	var symbols = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-	
+//generates a password depending on user input/interaction
+//returns a password with data type string
+function generatePassword() {
+  // prompts the user for an input between 8-128 (inclusive) and validates the input to ensure it's a number bettween 8-128 (inclusive)
+  // returns an integer which is the user input
+  function passwordLengthValidation() {
+    //prompts the user for a number between 8-128 (inclusive)
+    var userInput = prompt(
+      "How long would you like your password to be? (Length between 8 - 128 characters"
+    );
+    // validates if the user inputs a valid input
+    if (isNaN(userInput) == false && userInput >= 8 && userInput <= 128) {
+      return userInput;
+    } else {
+      //when user input is no valid, reccursive funcation call.
+      alert("Invalid entry, please try again (Pick a number between 8 - 128");
+      return passwordLengthValidation();
+    }
+  }
+  //confirm what types of characters the user wants to include in the password
+  //returns a string that includes all the characters that match the type of characters user has selected
+  function toInclude() {
+    var includeLowerCase = confirm(
+      "Would you like to include lowercase characters in your password?"
+    );
+    var includeUpperCase = confirm(
+      "Would you like to include uppercase characters in your password?"
+    );
+    var includeNumbers = confirm(
+      "Would you like to include numbers in your password?"
+    );
+    var includeSymbols = confirm(
+      "Would you like to include symbols in your password?"
+    );
+    //validates that at least one type of character is selected
+    if (
+      includeLowerCase ||
+      includeUpperCase ||
+      includeNumbers ||
+      includeSymbols
+    ) {
+      var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+      var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var numbers = "0123456789";
+      var symbols = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+      //string to concatenate different types of characters
+      var charactersToInclude = "";
+      //checks which types of characters the user selected and concatenates them into variable charactersToInclude
+      if (includeLowerCase == true) {
+        charactersToInclude += lowerCaseCharacters;
+      }
+      if (includeUpperCase == true) {
+        charactersToInclude += upperCaseCharacters;
+      }
+      if (includeNumbers == true) {
+        charactersToInclude += numbers;
+      }
+      if (includeSymbols == true) {
+        charactersToInclude += symbols;
+      }
+      //returns a string of all characters that match the character type the user selected
+      return charactersToInclude;
+    } else {
+      //recursive funcation call if no types of characters were not selected
+      alert("Please make sure at least one type is included");
+      return toInclude();
+    }
+  }
+  // calls passwordLengthValidation function and stores returned value into passwordLength
+  var passwordLength = passwordLengthValidation();
+  //calls toInclude function and stores returned value into characterList
+  var characterList = toInclude();
+  //empty string to concatenate characters randomly from characterList
+  var randomPassword = "";
+  for (var i = 0; i < passwordLength; i++) {
+    randomPassword +=
+      characterList[Math.floor(Math.random() * characterList.length)];
+  }
+  //returns password with data type string
+  return randomPassword;
 }
